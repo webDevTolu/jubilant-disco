ac14f1c1abd070ddd7aea425a5e9b427436a7cbb
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-04 17:01:37 +0000

    installed axios

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="App">App</div>;
+};
 
-export default App
+export default App;
